a4f599b5bf703eb1d92f3686ce83efb4bb75c49a
Michael Hari <michael_hari@apple.com>
2016-09-06 13:26:08 -0700

    (feat) Implementing lightbox next and previous buttons

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,15 @@
 
 var Slider = function () {
   var apiKey = 'c68800a17a432a39511ddfe44c78a500';
+  var app = document.body.querySelector('.app');
+
+  // DOM Nodes
+  var lightbox = document.body.querySelector('.lightbox');
+  var lightboxCloseButton = document.body.querySelector('.lightbox__button-close');
+  var lightboxPrevButton = document.body.querySelector('.lightbox__button-prev');
+  var lightboxNextButton = document.body.querySelector('.lightbox__button-next');
+  var lightboxImage = document.body.querySelector('.lightbox__image');
+
   var get = function get(url) {
     return new Promise(function (resolve, reject) {
       var req = new XMLHttpRequest();
@@ -19,30 +28,63 @@ var Slider = function () {
     });
   };
   // format data into sub-objects
-  var createImageObjects = function createImageObjects(images, id) {
-    // images.sizes.size.forEach((image) => {
-    //   if (image.label === "Large");
-    // })
+  var createImageObjects = function createImageObjects(images) {
     if (images.sizes.size.length > 8) {
+      var small = images.sizes.size[4].source;
       var medium = images.sizes.size[5].source;
       var large = images.sizes.size[8].source;
-      var smallAndMedium = {
-        id: id,
+      var smallMediumLarge = {
+        small: small,
         medium: medium,
         large: large
       };
-      return smallAndMedium;
+      return smallMediumLarge;
     }
     return;
   };
 
-  var log = function log(x) {
-    console.log(x);
+  var renderLightbox = function renderLightbox(node, imageSizes, prev, next) {
+    node.onclick = function () {
+      // Close Button
+      lightboxCloseButton.onclick = function () {
+        lightbox.classList.add('hide');
+      };
+      // Previous and Next Button
+      // TODO: MOVE BUTTONS TO PROMISE
+      if (typeof prev === 'undefined') {
+        lightboxPrevButton.classList.add('hide');
+      } else {
+        lightboxPrevButton.classList.remove('hide');
+      }
+      if (typeof next === 'undefined') {
+        lightboxNextButton.classList.add('hide');
+      } else {
+        lightboxNextButton.classList.remove('hide');
+      }
+
+      lightboxPrevButton.onclick = function () {
+        lightboxImage.src = prev.large;
+      };
+      lightboxNextButton.onclick = function () {
+        lightboxImage.src = next.large;
+      };
+      console.log('prev', prev);
+      console.log('next', next);
+      lightboxImage.src = imageSizes.large;
+      lightbox.classList.remove('hide');
+    };
   };
 
+  var renderThumbnails = function renderThumbnails(imageSizes) {
+    var img = document.createElement('img');
+    img.src = imageSizes.small;
+    img.classList.add('thumbnailImage');
+    return img;
+  };
+
+  // TODO - EXTRA CREDIT: build out custom search params with a default of "patterns"
   var fetchFromFlickr = function fetchFromFlickr() {
-    var photoId = 29158094390;
-    return Promise.resolve(get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&format=json&nojsoncallback=1&tags=patterns')).then(function (res) {
+    return Promise.resolve(get('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + apiKey + '&format=json&nojsoncallback=1&text=patterns&sort=relevance')).then(function (res) {
       var list = res.photos.photo.map(function (image) {
         return 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=' + apiKey + '&format=json&nojsoncallback=1&photo_id=' + image.id;
       });
@@ -51,14 +93,23 @@ var Slider = function () {
       var newList = list.map(function (url) {
         return Promise.resolve(get(url)).then(function (res) {
           var images = res;
-          var imageMap = createImageObjects(images, photoId);
+          var imageMap = createImageObjects(images);
           return imageMap;
         });
       });
       return Promise.all(newList);
     }).then(function (urls) {
-      // Do whatever you urls here
-      console.log(urls);
+      var imageContainer = document.createElement('div');
+      imageContainer.classList.add('images');
+      // Work with urls here
+      urls.forEach(function (imageSizes, index, arr) {
+        if (typeof imageSizes !== 'undefined') {
+          var img = renderThumbnails(imageSizes);
+          renderLightbox(img, imageSizes, arr[index - 1], arr[index + 1]);
+          imageContainer.appendChild(img);
+        }
+      });
+      app.appendChild(imageContainer);
     });
   };
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 const Slider = (() => {
   const apiKey = 'c68800a17a432a39511ddfe44c78a500';
+  const app = document.body.querySelector('.app');
+
+  // DOM Nodes
+  const lightbox = document.body.querySelector('.lightbox');
+  const lightboxCloseButton = document.body.querySelector('.lightbox__button-close');
+  const lightboxPrevButton = document.body.querySelector('.lightbox__button-prev');
+  const lightboxNextButton = document.body.querySelector('.lightbox__button-next');
+  const lightboxImage = document.body.querySelector('.lightbox__image');
+
   const get = (url) => new Promise((resolve, reject) => {
     const req = new XMLHttpRequest();
     req.open('GET', url);
@@ -13,30 +22,62 @@ const Slider = (() => {
     req.send();
   });
   // format data into sub-objects
-  const createImageObjects = (images, id) => {
-    // images.sizes.size.forEach((image) => {
-    //   if (image.label === "Large");
-    // })
+  const createImageObjects = (images) => {
     if (images.sizes.size.length > 8) {
+      const small = images.sizes.size[4].source;
       const medium = images.sizes.size[5].source;
       const large = images.sizes.size[8].source;
-      const smallAndMedium = {
-        id,
+      const smallMediumLarge = {
+        small,
         medium,
         large
       };
-      return smallAndMedium;
+      return smallMediumLarge;
     }
     return;
   };
 
-  const log = (x) => {
-    console.log(x);
+  const renderLightbox = (node, imageSizes, prev, next) => {
+    node.onclick = () => {
+      // Close Button
+      lightboxCloseButton.onclick = () => {
+        lightbox.classList.add('hide');
+      };
+      // Previous and Next Button
+      // TODO: MOVE BUTTONS TO PROMISE
+      if (typeof prev === 'undefined') {
+        lightboxPrevButton.classList.add('hide');
+      } else {
+        lightboxPrevButton.classList.remove('hide');
+      }
+      if (typeof next === 'undefined') {
+        lightboxNextButton.classList.add('hide');
+      } else {
+        lightboxNextButton.classList.remove('hide');
+      }
+
+      lightboxPrevButton.onclick = () => {
+        lightboxImage.src = prev.large;
+      }
+      lightboxNextButton.onclick = () => {
+        lightboxImage.src = next.large;
+      }
+      console.log('prev', prev);
+      console.log('next', next);
+      lightboxImage.src = imageSizes.large;
+      lightbox.classList.remove('hide');
+    };
   };
 
-  const fetchFromFlickr = () => {
-    const photoId = 29158094390;
-    return Promise.resolve(get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&format=json&nojsoncallback=1&tags=patterns`))
+  const renderThumbnails = (imageSizes) => {
+    const img = document.createElement('img');
+    img.src = imageSizes.small;
+    img.classList.add('thumbnailImage');
+    return img;
+  };
+
+  // TODO - EXTRA CREDIT: build out custom search params with a default of "patterns"
+  const fetchFromFlickr = () => Promise.resolve(get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&format=json&nojsoncallback=1&text=patterns&sort=relevance`))
     .then((res) => {
       const list = res.photos.photo.map((image) =>
        `https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=${apiKey}&format=json&nojsoncallback=1&photo_id=${image.id}`);
@@ -47,17 +88,25 @@ const Slider = (() => {
         Promise.resolve(get(url))
         .then((res) => {
           const images = res;
-          const imageMap = createImageObjects(images, photoId);
+          const imageMap = createImageObjects(images);
           return imageMap;
         })
       );
       return Promise.all(newList);
     })
     .then((urls) => {
-      // Do whatever you urls here
-      console.log(urls);
+      const imageContainer = document.createElement('div');
+      imageContainer.classList.add('images');
+      // Work with urls here
+      urls.forEach((imageSizes, index, arr) => {
+        if (typeof imageSizes !== 'undefined') {
+          const img = renderThumbnails(imageSizes);
+          renderLightbox(img, imageSizes, arr[index - 1], arr[index + 1]);
+          imageContainer.appendChild(img);
+        }
+      });
+      app.appendChild(imageContainer);
     });
-  };
 
   const init = () => {
     fetchFromFlickr();
